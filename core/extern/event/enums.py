c8f81e71123a89f13b03486b6dde4a9450aeb9bf
osennyaya <supersduby@gmail.com>
2021-02-27 15:51:22 +0800

    🌈style(pycache): 删除多余等pycache

--- core/extern/event/enums.py
+++ core/extern/event/enums.py
@@ -1,6 +1,6 @@
-from enum import Enum
+from enum import Enum, unique
 
-
+@unique
 class EventType(Enum):
     GroupMessageEvent = 'GroupMessageEvent'
     ContactMessageEvent = 'ContactMessageEvent'
--- core/extern/message/enums.py
+++ core/extern/message/enums.py
@@ -1,5 +1,6 @@
-from enum import Enum
+from enum import Enum, unique
 
+@unique
 class MessageType(Enum):
     AtMessage = 'At'
     TextMessage = 'Plain'
