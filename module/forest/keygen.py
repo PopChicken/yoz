21c1957de3f9c292c4dc5cc0fdd6095faf8b5efa
PopChicken <jimmy.jino@hotmail.com>
2021-03-11 23:53:44 +0800

    ✨feat(module.forest): 添加了key生成器模块
    为forest添加了key生成器模块，为加速卡实现提供基础。

---
+++ module/forest/keygen.py
@@ -0,0 +1,20 @@
+import uuid
+import pydblite
+
+from core.extern.data import Data
+
+
+def genKeys(num: int, effecct: float):
+    dataPath = Data('forest').getfo()
+    db = pydblite.Base(f'{dataPath}/keys.db')
+    if db.exists():
+        db.open()
+    else:
+        db.create('key', 'effect')
+        db.create_index('uuid')
+        db.commit()
+    
+    for i in range(0, num):
+        key = uuid.uuid4()
+        db.insert(key=key, effect=effecct)
+    db.commit()
